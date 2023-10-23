71981c263f1ddfba8b2278e353f6c8dc117f6b8a
desert0rluz <luizmartinsluz@gmail.com>
2023-10-23 16:35:31 -0300

    remoção de pasta

---
+++ caseTeste.py
@@ -0,0 +1,17 @@
+dia = 1
+
+match dia:
+    case 0:
+        print("segunda-feira")
+    case 1:
+        print("terça-feira")
+    case 2:
+        print("quarta-feira")
+    case 3:
+        print("quinta-feira")
+    case 4:
+        print("sexta-feira")
+    case 5:
+        print("sabado")
+    case _:
+         print("domingo")
